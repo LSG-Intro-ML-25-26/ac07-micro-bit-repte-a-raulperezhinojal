85c12a7a6cab2d324865193a350568095c1b63e7
Raul Pérez Hinojal <raul.perez@gracia.lasalle.cat>
2025-12-09 18:07:40 +0100

    piedra papel y tijera

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
-basic.forever(function () {
-	
+let hand = 0
+input.onGesture(Gesture.Shake, function () {
+    hand = randint(1, 3)
+    if (hand == 1) {
+        basic.showIcon(IconNames.SmallSquare)
+    } else if (hand == 2) {
+        basic.showIcon(IconNames.Square)
+    } else {
+        basic.showIcon(IconNames.Scissors)
+    }
 })
